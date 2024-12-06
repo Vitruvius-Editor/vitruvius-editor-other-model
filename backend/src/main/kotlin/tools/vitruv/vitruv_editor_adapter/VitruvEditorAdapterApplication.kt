b46cf186930aa01fdf550437f51722e8b207c54b
Yannis Storrer <yannis.storrer@web.de>
2024-12-06 16:30:31 +0100

    Add frontend and backend folders
    Add an empty frontend folder and a backend folder with a template
    generated by spring initialiser.

---
+++ backend/src/main/kotlin/tools/vitruv/vitruv_editor_adapter/VitruvEditorAdapterApplication.kt
@@ -0,0 +1,11 @@
+package tools.vitruv.vitruv_editor_adapter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VitruvEditorAdapterApplication
+
+fun main(args: Array<String>) {
+	runApplication<VitruvEditorAdapterApplication>(*args)
+}
---
+++ backend/src/test/kotlin/tools/vitruv/vitruv_editor_adapter/VitruvEditorAdapterApplicationTests.kt
@@ -0,0 +1,13 @@
+package tools.vitruv.vitruv_editor_adapter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VitruvEditorAdapterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
