054a391a68146d13f2e7954be20a2413b5e10959
Nico <Nicolas.Adelmann@icloud.com>
2024-12-13 21:34:16 +0100

    change String to Content

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/logic/api/displayViews/ViewMapper.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/logic/api/displayViews/ViewMapper.kt
@@ -1,10 +1,9 @@
 package tools.vitruv.vitruvAdapter.logic.api.displayViews
 
 import org.eclipse.emf.ecore.EObject
-
+import tools.vitruv.vitruvAdapter.logic.api.contents.Content
 
 /**
- * THIS CLASS IS ONLY AN IDEA AND NOT FINAL
  * This interface represents a view mapper, which is able to map contents of a view (RootObjects/EObjects),
  * to things that can be displayed in the graphical editor. These are represented as json contents, which the graphical editor can interpret.
  * @author
@@ -16,12 +15,12 @@ interface ViewMapper {
      * @param viewContent The view content to map.
      * @return The json string representing the view content.
      */
-    fun mapViewContent(viewContent: Any): String
+    fun mapViewContent(viewContent: List<EObject>): Content
 
     /**
      * Maps the given json string to a view content, which can be displayed in the graphical editor.
      * @param json The json string to map.
      * @return The view content.
      */
-    fun mapViewContent(json: String): List<EObject>
+    fun mapViewContent(viewContent: Content): List<EObject>
 }
