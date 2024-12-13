f53dd05702f7f368d25d9bfd05d75a35e95dbbd5
Nico <Nicolas.Adelmann@icloud.com>
2024-12-13 21:33:14 +0100

    add comments and add getViewMapper() method

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/logic/api/displayViews/DisplayView.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/logic/api/displayViews/DisplayView.kt
@@ -4,11 +4,21 @@ import tools.vitruv.framework.views.ViewSelector
 import tools.vitruv.framework.views.ViewType
 
 /**
- * THIS CLASS IS ONLY AN IDEA AND NOT FINAL
  * This interface represents a display view, which can be displayed in the Vitruvius graphical editor.
  * @author uhsab
  */
 interface DisplayView {
 
+    /**
+     * Gets the relating vitruvius view type.
+     * @return The relating vitruvius view type.
+     */
     fun getViewType(): ViewType<out ViewSelector>
+
+    /**
+     * Gets the view mapper of the display view.
+     * @return The view mapper of the display view.
+     */
+    fun getViewMapper(): ViewMapper
+
 }
