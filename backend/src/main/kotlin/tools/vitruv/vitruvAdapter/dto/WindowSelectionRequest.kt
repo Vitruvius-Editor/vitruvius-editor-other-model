0e0de9b1996778b671f9cc4cf042718b2ea98606
Yannis Storrer <yannis.storrer@web.de>
2025-01-04 23:03:44 +0100

    Fix json serialisation of selector dto

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/dto/WindowSelectionRequest.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/dto/WindowSelectionRequest.kt
@@ -1,7 +1,18 @@
 package tools.vitruv.vitruvAdapter.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = WindowSelectionRequest.All::class, name = "All"),
+    JsonSubTypes.Type(value = WindowSelectionRequest.Name::class, name = "Name")
+)
 sealed class WindowSelectionRequest() {
     data object All : WindowSelectionRequest()
 
