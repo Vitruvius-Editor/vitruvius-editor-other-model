e8e42c8c2e02408b269ebe4408d31771c6a68798
Nico <Nicolas.Adelmann@icloud.com>
2025-01-20 18:50:51 +0100

    delete unnecessary imports

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/vitruv/api/DisplayView.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/vitruv/api/DisplayView.kt
@@ -1,8 +1,5 @@
 package tools.vitruv.vitruvAdapter.vitruv.api
 
-import tools.vitruv.framework.views.ViewSelector
-import tools.vitruv.framework.views.ViewType
-
 /**
  * This interface represents a display view, which can be displayed in the Vitruvius graphical editor.
  * @author uhsab
