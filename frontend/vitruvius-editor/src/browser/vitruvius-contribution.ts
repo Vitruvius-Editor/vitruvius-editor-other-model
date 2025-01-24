aabafba5cbc98a94231d1110da1bc8058eb0b44c
Yannis Storrer <yannis.storrer@web.de>
2025-01-24 14:49:01 +0100

    Implement edit project functionality

--- frontend/vitruvius-editor/src/browser/vitruvius-contribution.ts
+++ frontend/vitruvius-editor/src/browser/vitruvius-contribution.ts
@@ -157,9 +157,38 @@ export class VitruviusEditProjectContribution implements CommandContribution {
   protected readonly messageService!: MessageService;
   @inject(ConnectionService)
   protected readonly connectionService!: ConnectionService;
+  @inject(QuickInputService)
+  protected readonly quickInputService!: QuickInputService;
+  @inject(QuickPickService)
+  protected readonly quickPickService!: QuickPickService;
+  @inject(DisplayViewWidgetContribution)
+  protected readonly displayViewWidgetContribution!: DisplayViewWidgetContribution;
   registerCommands(registry: CommandRegistry): void {
     registry.registerCommand(VitruviusEditProject, {
-      execute: () => this.messageService.info("Edit Project"),
+      execute: () => this.connectionService.getConnections().then(connections => {
+		  let items = connections.map(connection => {
+			return {
+				label: connection.name,
+				execute: () => {
+					this.quickInputService.input({title: 'Edit the projects name.', value: connection.name}).then(name => {
+						this.quickInputService.input({title: 'Edit the projects description.', value: connection.description}).then(description => {
+							this.quickInputService.input({title: 'Edit the projects URL', value: connection.url}).then(url => {
+								this.connectionService.updateConnection(connection.uuid, {name, description, url}).then(newConnection => {
+									this.messageService.info('Project sucessfully updated.');
+									this.displayViewWidgetContribution.widget.then(widget => {
+										if (widget.getConnection()?.uuid == newConnection.uuid) {
+											widget.loadProject(newConnection);
+										}
+									})								
+								}).catch(_err => this.messageService.error("Couldn't connect to Backend server."))
+							})
+						})
+					});
+				}
+			}
+		  });
+		  this.quickPickService.show(items);
+	  }),
     });
   }
 }
