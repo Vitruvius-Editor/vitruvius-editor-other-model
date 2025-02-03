d5cd52ef639e9e025661410bb3d5e936d51a2bfe
Nico <Nicolas.Adelmann@icloud.com>
2025-02-03 16:12:29 +0100

    fix ViewMapper for PackageDiagramViewMapper.kt

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlPackageView/PackageDiagramViewMapper.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlPackageView/PackageDiagramViewMapper.kt
@@ -4,9 +4,15 @@ import org.eclipse.emf.ecore.EObject
 import tools.vitruv.vitruvAdapter.core.api.DisplayContentMapper
 import tools.vitruv.vitruvAdapter.core.api.Window
 import tools.vitruv.vitruvAdapter.core.impl.abstractMapper.UmlViewMapper
+import tools.vitruv.vitruvAdapter.core.impl.uml.UmlDiagram
 
 class PackageDiagramViewMapper: UmlViewMapper() {
-    override fun mapEObjectsToWindowsContent(rootObjects: List<EObject>): List<Window<String>> {
+    /**
+     * Maps the given view content to a list of windows.
+     * @param selectEObjects The view content to map.
+     * @return The windows representing the view content.
+     */
+    override fun mapEObjectsToWindowsContent(selectEObjects: List<EObject>): List<Window<UmlDiagram>> {
         TODO("Not yet implemented")
     }
 
@@ -20,17 +26,26 @@ class PackageDiagramViewMapper: UmlViewMapper() {
      */
     override fun mapWindowsToEObjectsAndApplyChangesToEObjects(
         oldEObjects: List<EObject>,
-        windows: List<Window<String>>
+        windows: List<Window<UmlDiagram>>
     ): List<EObject> {
         TODO("Not yet implemented")
     }
 
-
+    /**
+     * Maps the given view to all windows it can find within the view.
+     * @param rootObjects The view to map.
+     * @return The names of the windows that are available in the view.
+     */
     override fun mapViewToWindows(rootObjects: List<EObject>): Set<String> {
         TODO("Not yet implemented")
     }
 
-    override fun getDisplayContent(): DisplayContentMapper<String> {
+    /**
+     * Gets the display content of this view mapper, which is able to map the view content to a json string and vice versa.
+     * @return The display content of this view mapper.
+     */
+    override fun getDisplayContent(): DisplayContentMapper<UmlDiagram> {
         TODO("Not yet implemented")
     }
+
 }
