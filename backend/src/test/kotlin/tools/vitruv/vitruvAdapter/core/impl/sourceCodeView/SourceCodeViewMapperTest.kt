fb76da6b1cbd0e6fa15d2fe202cea565cb938846
uvndx <amiralayoubi.aa@gmail.com>
2025-02-03 23:52:13 +0100

    fix SourceCodeViewMapperTest.kt

--- backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/sourceCodeView/SourceCodeViewMapperTest.kt
+++ backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/sourceCodeView/SourceCodeViewMapperTest.kt
@@ -1,6 +1,10 @@
 package tools.vitruv.vitruvAdapter.core.impl.sourceCodeView
 
 import org.eclipse.emf.ecore.EObject
+import org.eclipse.jdt.core.ToolFactory
+import org.eclipse.jdt.core.formatter.CodeFormatter
+import org.eclipse.jface.text.Document
+import org.eclipse.text.edits.TextEdit
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -35,27 +39,52 @@ class SourceCodeViewMapperTest {
         val selectedEObjects = sourceCodeSelector.applySelection(eObjects, views)
         val selectedContent = mapper.mapEObjectsToWindowsContent(selectedEObjects)
         val firstClassContent = selectedContent[0].content
-        val expectedFirstClassContent = "public class TestClass {\n" +
-                "int a = 0;\n" +
+        val expectedFirstClassContent = "public class TestClass {" +
                 "\n" +
-                "int b = 1;\n" +
+                "    int a = 0;\n" +
                 "\n" +
-                "int c = 2 + 54;\n" +
+                "    int b = 1;\n" +
                 "\n" +
-                "public  void testMethod() {\n" +
-                "System.out.println(\"Hello World!\");\n" +
+                "    int c = 2 +54;\n" +
+                "\n" +
+                "    public void testMethod() {\n" +
+                "        System.out.println(\"Hello World!\");\n" +
+                "    }\n" +
+                "\n" +
+                "    public void testMethod2() {\n" +
+                "        System.out.println(\"Hello World!\");\n" +
+                "    }\n" +
+                "\n" +
+                "    public void testMethod3() {\n" +
+                "        System.out.println(\"Hello World!\");\n" +
+                "    }\n" +
+                "\n" +
                 "}\n" +
-                "\n" +
-                "public  void testMethod2() {\n" +
-                "System.out.println(\"Hello World!\");\n" +
-                "}\n" +
-                "\n" +
-                "public  void testMethod3() {\n" +
-                "System.out.println(\"Hello World!\");\n" +
-                "}\n" +
-                "\n" +
-                "}\n"
-        assertEquals(expectedFirstClassContent, firstClassContent)
+                "\n"
+
+        assertEquals(formatJavaCode(expectedFirstClassContent), firstClassContent)
+    }
+
+    fun formatJavaCode(code: String): String? {
+        // Create a CodeFormatter with default options (you can also supply a map for custom settings)
+        val formatter: CodeFormatter = ToolFactory.createCodeFormatter(null)
+        // Format the code as a compilation unit (use CodeFormatter.K_COMPILATION_UNIT)
+        val textEdit: TextEdit? = formatter.format(
+            CodeFormatter.K_COMPILATION_UNIT,
+            code,
+            0,
+            code.length,
+            0,
+            null
+        )
+
+        return (if (textEdit != null) {
+            val document = Document(code)
+            textEdit.apply(document)
+            document.get()
+        } else {
+            null // formatting failed
+        }).toString()
     }
 
     @Test
