e9147b38b0d770711d719ff6828701315ee7363a
Nico <Nicolas.Adelmann@icloud.com>
2025-02-03 13:41:07 +0100

    fix generic type of uml view mapper

--- backend/src/main/kotlin/tools/vitruv/vitruvAdapter/core/impl/abstractMapper/UmlViewMapper.kt
+++ backend/src/main/kotlin/tools/vitruv/vitruvAdapter/core/impl/abstractMapper/UmlViewMapper.kt
@@ -1,5 +1,6 @@
 package tools.vitruv.vitruvAdapter.core.impl.abstractMapper
 
 import tools.vitruv.vitruvAdapter.core.api.ViewMapper
+import tools.vitruv.vitruvAdapter.core.impl.uml.UmlDiagram
 
-abstract class UmlViewMapper: ViewMapper<String>
+abstract class UmlViewMapper: ViewMapper<UmlDiagram>
