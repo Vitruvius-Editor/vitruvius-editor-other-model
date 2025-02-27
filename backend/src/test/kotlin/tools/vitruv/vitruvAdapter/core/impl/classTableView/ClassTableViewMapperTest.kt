16c9e766beff1155465c56043872aac264de4c5e
uvndx <amiralayoubi.aa@gmail.com>
2025-02-27 16:36:59 +0100

    start testing ClassTableViewMapperTest.kt

--- backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/classTableView/ClassTableViewMapperTest.kt
+++ backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/classTableView/ClassTableViewMapperTest.kt
@@ -1,6 +1,11 @@
 package tools.vitruv.vitruvAdapter.core.impl.classTableView
 
+import org.eclipse.emf.ecore.EObject
 import org.junit.jupiter.api.Test
+import tools.vitruv.vitruvAdapter.core.api.PreMappedWindow
+import tools.vitruv.vitruvAdapter.core.impl.table.TableDTO
+import tools.vitruv.vitruvAdapter.utils.EObjectContainer
+import kotlin.test.assertEquals
 
 class ClassTableViewMapperTest {
   private val mapper = ClassTableViewMapper()
@@ -10,7 +15,8 @@ class ClassTableViewMapperTest {
    */
   @Test
   fun testMapToWindows() {
-
+    val windows = mapper.mapViewToWindows(EObjectContainer.getContainer3AsRootObjects())
+    assertEquals(setOf<String>("examplePackage"), windows)
   }
 
   /**
@@ -18,7 +24,11 @@ class ClassTableViewMapperTest {
    */
   @Test
   fun testMapEObjectsToWindowsContent() {
-
+    val preMappedWindow1 = PreMappedWindow<TableDTO<ClassTableEntry>>("examplePackage",
+        EObjectContainer.getContainer3AsRootObjects() as MutableList<EObject>
+    )
+    val window1 = mapper.mapEObjectsToWindowsContent(listOf(preMappedWindow1))
+    print(window1)
   }
 
   /**
