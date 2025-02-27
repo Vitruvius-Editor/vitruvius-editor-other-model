d4558e1995c7226e50fffa36abe8b542233cbfc6
uvndx <amiralayoubi.aa@gmail.com>
2025-02-27 15:41:46 +0100

    ClassTableContentSelectorTest.kt

--- backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/classTableView/ClassTableContentSelectorTest.kt
+++ backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/classTableView/ClassTableContentSelectorTest.kt
@@ -1,13 +1,26 @@
 package tools.vitruv.vitruvAdapter.core.impl.classTableView
 
-import org.junit.jupiter.api.Assertions.*
+import org.eclipse.emf.ecore.EObject
+import org.junit.jupiter.api.Assertions.assertEquals
+import tools.mdsd.jamopp.model.java.containers.CompilationUnit
+import tools.vitruv.vitruvAdapter.core.api.PreMappedWindow
+import tools.vitruv.vitruvAdapter.core.impl.table.TableDTO
+import tools.vitruv.vitruvAdapter.utils.EObjectContainer
+
 class ClassTableContentSelectorTest {
     private val selector = ClassTableContentSelector()
 
- /**
-  * @author Nico
-  */
- @org.junit.jupiter.api.Test
+    /**
+     * @author Nico & Amir
+     */
+    @org.junit.jupiter.api.Test
     fun testApplySelection() {
+        val javaPackage = EObjectContainer.getContainer1AsRootObjects()
+        val selectedObjects = selector.applySelection(javaPackage, setOf("examplePackage"))
+        val classTableWindow = PreMappedWindow<TableDTO<ClassTableEntry>>("examplePackage", javaPackage as MutableList<EObject>)
+        val expectedSelectedWindows = listOf(
+            classTableWindow
+        )
+        assertEquals(expectedSelectedWindows, selectedObjects)
     }
 }
