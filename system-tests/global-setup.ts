d6f93c2ec539427d51aa04f21aed4feea1f6bd3a
Yannis Storrer <yannis.storrer@web.de>
2025-03-01 20:18:42 +0100

    Migrate to playwright

--- system-tests/global-setup.ts
+++ system-tests/global-setup.ts
@@ -1,20 +1,12 @@
 import Docker from 'dockerode';
 const docker = new Docker();
-jest.setTimeout(120000);
 
-beforeAll(async () => {
-    await pullImages();
-})
+export default async () => {
+   await pullImages();
+   await startContainers();
+   await waitForHealth("http://localhost:8080/actuator/health");
+}
 
-beforeEach(async () => {
-    await startContainers();
-    await waitForHealth("http://localhost:8080/actuator/health");
-    console.log("Backend is helathy.")
-})
-
-afterEach(async () => {
-    await stopContainers();
-})
 
 async function pullImages() {
     // Define the images
@@ -119,28 +111,7 @@ async function startContainers() {
     console.log('Server container started successfully:', serverContainer.id);
 }
 
-async function stopContainers() {
-    const containerNames = [
-        'vitruvius-editor-backend-test',
-        'vitruvius-editor-frontend-test',
-        'vitruvius-server-test'
-    ];
-
-    for (const name of containerNames) {
-        try {
-            const container = docker.getContainer(name);
-            await container.stop();
-            console.log(`Container ${name} stopped successfully.`);
-
-            await container.remove();
-            console.log(`Container ${name} removed successfully.`);
-        } catch (error) {
-            console.error(`Error stopping or removing container ${name}:`, error);
-        }
-    }
-}
-
-async function waitForHealth(url: string, timeout: number = 30000, interval: number = 5000) {
+async function waitForHealth(url: string, timeout: number = 60000, interval: number = 5000) {
     const startTime = Date.now();
 
     while (Date.now() - startTime < timeout) {
--- system-tests/src/example.test.ts
+++
@@ -1,3 +0,0 @@
-test('tests if jest is installed', () => {
-    expect(1).toBe(1)
-})
