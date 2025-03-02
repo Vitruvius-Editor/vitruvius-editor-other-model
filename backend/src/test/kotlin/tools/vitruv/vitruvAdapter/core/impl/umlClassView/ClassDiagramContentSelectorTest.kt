140b9bff94d306a01fec66a733e21101774b126e
uvndx <amiralayoubi.aa@gmail.com>
2025-03-02 02:21:05 +0100

    add ClassDiagramContentSelectorTest.kt tests

--- backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlClassView/ClassDiagramContentSelectorTest.kt
+++ backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlClassView/ClassDiagramContentSelectorTest.kt
@@ -1,7 +1,9 @@
 package tools.vitruv.vitruvAdapter.core.impl.umlClassView
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import tools.vitruv.vitruvAdapter.core.impl.sourceCodeView.SourceCodeContentSelector
+import tools.vitruv.vitruvAdapter.core.api.PreMappedWindow
+import tools.vitruv.vitruvAdapter.core.impl.uml.UmlDiagram
 import tools.vitruv.vitruvAdapter.utils.EObjectContainer
 
 class ClassDiagramContentSelectorTest {
@@ -9,9 +11,20 @@ class ClassDiagramContentSelectorTest {
 
 
  /**
-  * @author Patrick
+  * @author Patrick & Amir
   */
  @Test
  fun testApplySelection() {
+  val container = EObjectContainer.getContainerWith2Packages()
+  val selectedObjects = selector.applySelection(container, setOf("examplePackage", "examplePackage2"))
+  val umlPackage1 = container[2]
+  val umlPackage2 = container[3]
+  val classTableWindow = PreMappedWindow<UmlDiagram>("examplePackage", mutableListOf(umlPackage1))
+    val classTableWindow2 = PreMappedWindow<UmlDiagram>("examplePackage2", mutableListOf(umlPackage2))
+  val expectedSelectedWindows = listOf(
+   classTableWindow,
+    classTableWindow2
+  )
+  assertEquals(expectedSelectedWindows, selectedObjects)
  }
 }
