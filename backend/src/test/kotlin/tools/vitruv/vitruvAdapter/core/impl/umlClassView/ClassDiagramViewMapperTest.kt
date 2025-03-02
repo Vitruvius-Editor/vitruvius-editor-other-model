fc8a4e6ca35521b88dfc8ae76b63d4585a8eeea6
uvndx <amiralayoubi.aa@gmail.com>
2025-03-02 02:39:47 +0100

    add simple ClassDiagramViewMapperTest.kt test

--- backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlClassView/ClassDiagramViewMapperTest.kt
+++ backend/src/test/kotlin/tools/vitruv/vitruvAdapter/core/impl/umlClassView/ClassDiagramViewMapperTest.kt
@@ -1,17 +1,22 @@
 package tools.vitruv.vitruvAdapter.core.impl.umlClassView
-
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import tools.vitruv.vitruvAdapter.core.api.PreMappedWindow
+import tools.vitruv.vitruvAdapter.core.api.Window
+import tools.vitruv.vitruvAdapter.core.impl.uml.UmlDiagram
+import tools.vitruv.vitruvAdapter.utils.EObjectContainer
 
 class ClassDiagramViewMapperTest {
 
  private val mapper = ClassDiagramViewMapper()
 
  /**
-  * @author Patrick
+  * @author Patrick & Amir
   */
  @Test
  fun testMapToWindows() {
-
+  val windows = mapper.mapViewToWindows(EObjectContainer.getContainer3AsRootObjects())
+  assertEquals(setOf<String>("examplePackage"), windows)
  }
 
  /**
@@ -19,7 +24,9 @@ class ClassDiagramViewMapperTest {
   */
  @Test
  fun testMapEObjectsToWindowsContent() {
-
+    val preMappedWindow1 = PreMappedWindow<UmlDiagram>("examplePackage", EObjectContainer.getContainer3AsRootObjects().toMutableList())
+    val window1 = mapper.mapEObjectsToWindowsContent(listOf(preMappedWindow1))
+    print(window1)
  }
 
  /**
