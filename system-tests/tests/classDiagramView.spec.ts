1eec6036de5b919aa1fe0e49580f1a81a988a7bd
Yannis Storrer <yannis.storrer@web.de>
2025-03-11 21:43:37 +0100

    Add test for class diagram deletion

--- system-tests/tests/classDiagramView.spec.ts
+++ system-tests/tests/classDiagramView.spec.ts
@@ -51,3 +51,28 @@ test('Test class diagram editing', async ({ page }) => {
   await expect(page.locator('[id="packagediagramwidget\\:packagediagramwidget"]')).toContainText('ClassFoo +myIntAttribute:int - +myIntAttribute:Object - +myOperationTest(param1:int - ,param2:int - ):int - ClassBar +myIntAttributeTest:Object - +myOperation():Object -');
   await expect(page.locator('[id="packagediagramwidget\\:packagediagramwidget"]')).toContainText('ClassBar +myIntAttributeTest:Object - +myOperation():Object -');
 });
+
+test('Test class diagram deleting', async ({ page }) => {
+  await page.goto('http://localhost:3000/#/home/project');
+  await page.locator('[id="theia\\:menubar"]').getByText('Vitruvius').click();
+  await page.getByText('Vitruvius Import Project').click();
+  await page.getByRole('combobox', { name: 'input' }).fill('Example Project');
+  await page.getByRole('combobox', { name: 'input' }).press('Enter');
+  await page.getByRole('combobox', { name: 'input' }).fill('Example Description');
+  await page.getByRole('combobox', { name: 'input' }).press('Enter');
+  await page.getByRole('combobox', { name: 'input' }).fill('localhost');
+  await page.getByRole('combobox', { name: 'input' }).press('Enter');
+  await page.getByRole('combobox', { name: 'input' }).fill('8000');
+  await page.getByRole('combobox', { name: 'input' }).press('Enter');
+  await page.locator('[id="shell-tab-widget\\:display-views"] > .theia-tab-icon-label > .p-TabBar-tabIcon').click();
+  await page.getByText('ClassDiagram').click();
+  await page.getByText('examplePackage').click();
+  await page.locator('span:nth-child(23)').click();
+  await page.locator('span:nth-child(13)').click();
+  await page.getByText('-', { exact: true }).nth(1).click();
+  await page.locator('[id="theia\\:menubar"]').getByText('Vitruvius').click();
+  await page.getByText('Vitruvius Refresh Project', { exact: true }).click();
+  await page.getByRole('option', { name: 'ClassDiagram examplePackage' }).locator('a').click();
+  await expect(page.locator('[id="packagediagramwidget\\:packagediagramwidget"]')).toContainText('Class1 +myIntAttribute:int - +myOperation(param1:int - ):int -');
+  await expect(page.locator('[id="packagediagramwidget\\:packagediagramwidget"]')).toContainText('Class2 +myIntAttribute:Object -');
+});
